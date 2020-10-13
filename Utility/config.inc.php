da5d22b43eeadd2ac7e174046ac166e2358720f4
AlessandroStefanachi <alestefa98@gmail.com>
2020-10-13 17:42:15 +0200

    config file

---
+++ Utility/config.inc.php
@@ -0,0 +1,5 @@
+<?php global $config;
+$config['mysql']['typedb']= 'mysql:host=127.0.0.1;dbname=TVtracker';
+$config['mysql']['password']=  '1234';
+$config['mysql']['user']= 'root';
+$config['mysql']['host']= '127.0.0.1';?>
