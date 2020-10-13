01ccae87c51fa69d3165eadcd726c1515552d96a
AlessandroStefanachi <alestefa98@gmail.com>
2020-10-13 18:23:28 +0200

    Persisten manager prima versione

--- Foundation/FConnectionDB.php
+++ Foundation/FConnectionDB.php
@@ -1,7 +1,227 @@
 <?php
+require_once 'Utility/config.inc.php';
+
+class FConnectionDB {
+
+    /** istanza unica della classe */
+    private static $istanza;
+
+    /** oggetto PDO che effettua la connessione al dbms */
+    private $pdo;
+
+    /** il costruttore è privato ed è chiamato solo da getIstanza() una volta sola */
+    private function __construct() {
+        global $config;
+        try {
+            $this->pdo = new PDO($config['mysql']['typedb'],$config['mysql']['user'],$config['mysql']['password']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo "Errore: " . $e->getMessage();
+            die;
+        }
+    }
+
+    /**
+     * Metodo che restituisce l'unica istanza dell'oggetto.
+     * @return FDataBase l'istanza dell'oggetto.
+     */
+    public static function getIstanza() {
+        if(self::$istanza == null) self::$istanza = new FConnectionDB();
+        return self::$istanza;
+    }
+
+    /**
+     * Metodo che permette di salvare informazioni contenute in un oggetto Entity sul database.
+     * @param $oggetto oggetto da salvare
+     * @param $nomeClasse nome della classe che serve per ottenere il nome della tabella e i nomi dei campi per fare la query
+     */
+    public function store($oggetto, $nomeClasse) {
+
+        try {
+
+            $this->pdo->beginTransaction();
+            $sql = "INSERT INTO ". $nomeClasse::getNomeTabella() . $nomeClasse::getCampiTabella() . " VALUES " . $nomeClasse::getCampiParametriciTabella();
+            $stmt = $this->pdo->prepare($sql);
+            $nomeClasse::bind($stmt, $oggetto);
+            $stmt->execute();
+            $id = $this->pdo->lastInsertId();
+            $this->pdo->commit();
+            $this->closeDBConnection();
+            return $id;
+
+        } catch (Exception $e) {
+            echo "Errore: " . $e->getMessage();
+            $this->pdo->rollBack();
+            return null;
+        }
+    }
+
+    /**
+     * Metodo che permette di prelevare uno o più oggetti dal db
+     * @param $campo nome del campo
+     * @param $campo valore del campo
+     * @param $nomeTabella nome della tabella su cui fare la query
+     */
+    public function load($campo, $valoreCampo, $nomeTabella) {
+
+        try {
+
+            $sql = "SELECT * FROM " . $nomeTabella . " WHERE " . $campo . " = '" . $valoreCampo . "';";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            $risultato = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $numeroRighe = $stmt->rowCount();
+            if($numeroRighe == 0) {
+                $risultato = NULL;
+            }
+            return $risultato;
+
+        } catch (Exception $e) {
+            echo "Errore: " . $e->getMessage();
+            $this->pdo->rollBack();
+        }
+    }
+
+    /**
+     * Metodo che permette di aggiornare il valore di un attributo passato come parametro
+     * @param $campo campo da aggiornare
+     * @param $nuovoValore nuovo valore da inserire
+     * @param $chiave nome del campo chiave primaria della classe interessata
+     * @param $id valore della chiave primaria
+     * @param $nomeClasse nome della classe interessata
+     */
+    public function update ($campo, $nuovoValore, $chiave, $id, $nomeClasse) {
+        try {
+            $this->pdo->beginTransaction();
+            $sql = "UPDATE " . $nomeClasse::getNomeTabella() . " SET " . $campo . "='" . $nuovoValore . "' WHERE " . $chiave . "='" . $id . "';";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            $this->pdo->commit();
+            $this->closeDbConnection();
+            return true;
+        } catch (PDOException $e) {
+            echo "Errore: " . $e->getMessage();
+            $this->pdo->rollBack();
+            return false;
+        }
+    }
+
+    /**
+     * Metodo che verifica l'esistenza di un campo con un certo valore nella tabella $nomeTabella
+     * @param $campo campo da aggiornare
+     * @param $valoreCampo valore del campo
+     * @param $nomeTabella nome della tabella interessata
+     */
+    public function exist($campo, $valoreCampo, $nomeTabella) {
+        try {
+
+            $sql = "SELECT * FROM " . $nomeTabella . " WHERE " . $campo . " = '" . $valoreCampo . "';";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            $risultato = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $numeroRighe = $stmt->rowCount();
+            if($numeroRighe == 0) {
+                $risultato = NULL;
+            }
+            return $risultato;
+
+        } catch (Exception $e) {
+            echo "Errore: " . $e->getMessage();
+            $this->pdo->rollBack();
+        }
+    }
 
 
-class FConnectionDB
-{
+    public function loadVerificaAccessoPWHash ($username, $pass) {
+
+        try {
+            $sql = null;
+            $class = "FUtenteRegistrato";
+            $sql = "SELECT * FROM " . $class::getNomeTabella() . " WHERE username = :us";
+            //print($sql);
+            $stmt = $this->pdo->prepare($sql);
+            // $stmt->execute();
+            $stmt->execute(array(':us' => $username));
+            $numeroRighe = $stmt->rowCount();
+            if ($numeroRighe == 0) {
+                $risultato = null;
+            } else {
+                $risultato = $stmt->fetch(PDO::FETCH_ASSOC);   //ritorna una sola riga
+                if(!password_verify($pass,$risultato["password"])) $risultato = null; //se la passw hashata non corrisponde
+            }
+            return $risultato;
+        } catch (PDOException $e) {
+            echo "Errore: " . $e->getMessage();
+            $this->db->rollBack();
+            return null;
+        }
+    }
+
+    /**
+     * Metodo che permette di eliminare un commento nel db
+     * @param id id usato per la cancellazione
+     * @return $verifica bool se la cancellazione è andata a buon fine
+     */
+    public function deleteCommento($id) {
+        try {
+            $verifica = null;
+            $this->pdo->beginTransaction();
+
+            $sql = "DELETE FROM Commento WHERE id ='" . $id . "';";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            $this->pdo->commit();
+            $this->closeDbConnection();
+            $verifica = true;
+
+        } catch (PDOException $e) {
+            echo "Attenzione errore: " . $e->getMessage();
+            $this->pdo->rollBack();
+            //return false;
+        }
+        return $verifica;
+    }
+
+    /** Chiude la connessione con il db */
+    public function closeDBConnection() {
+        self::$istanza = null;
+    }
 
 }
+
+/*
+NB: $stmt->fetch() ritorna un array di elementi e bisogna usare un indice per accedere ad un elemento dell'array
+    $stmt->fetchAll() ritorna un array di array e bisogna usare due indici
+*/
+
+/**
+ * Metodo che permette di verificare se l'utente è nel db e restituisce la riga (array) contenente l'utente
+ * inoltre verifica l'accesso con prevenzione dall'sql injection
+ * @param $username
+ * @param $pass
+ */
+/*
+public function loadVerificaAccesso ($username, $pass) {
+
+    try {
+        $sql = null;
+        $class = "FUtenteRegistrato";
+        $sql = "SELECT * FROM " . $class::getNomeTabella() . " WHERE username = :us AND password = :pw";
+        //print($sql);
+        $stmt = $this->pdo->prepare($sql);
+       // $stmt->execute();
+        $stmt->execute(array(':us' => $username,':pw' => $pass));
+        $numeroRighe = $stmt->rowCount();
+        if ($numeroRighe == 0) {
+            $risultato = null;
+        } else {
+            $risultato = $stmt->fetch(PDO::FETCH_ASSOC);   //ritorna una sola riga
+        }
+        return $risultato;
+    } catch (PDOException $e) {
+        echo "Errore: " . $e->getMessage();
+        $this->db->rollBack();
+        return null;
+    }
+}
+*/
---
+++ Foundation/FPersistenManager.php
@@ -0,0 +1,83 @@
+<?php
+
+class FPersistentManager {
+
+    /**
+     *Metodo che permette di salvare un oggetto sul db
+     *@param $oggetto oggetto da salvare
+     */
+    public static function store($oggetto) {
+        $EClass = get_class($oggetto);
+        $FClass = str_replace('E', 'F', $EClass);
+        $FClass::store($oggetto);
+    }
+
+    /**
+     *Metodo che permette di prelevare uno o più oggetti dal db
+     *@param $campo campo della tabella interessata
+     *@param $valoreCampo il valore del campo
+     *@param $nomeClasse il nome della classe foundation da richiamare per prelevare l'/gli oggetto/i
+     */
+    public static function load($campo, $valoreCampo, $nomeClasse) {
+        $risultato = $nomeClasse::load($campo, $valoreCampo);
+        return $risultato;
+    }
+
+
+
+    /**
+     *Metodo che permette di prelevare un utente registrato dal db
+     *richiamando però il metodo che verifica la corrispondenza della password inserita(in chiaro)
+     *tramite post e la password hashata nel db
+     *@param $username
+     *@param $pass è la password
+     */
+    public static function loadLoginPWHash($username, $pass) {
+        $risultato = null;
+        $risultato = FUtenteRegistrato::loadLoginPWHash($username, $pass);
+        return $risultato;
+    }
+
+    /** verifica è boolean e ci dice se la richiesta di update è andata a buon fine */
+    public static function update($campo, $nuovoValore, $chiave, $id, $nomeClasse) {
+        if($nomeClasse == "FPartita" || $nomeClasse == "FUtenteRegistrato" || $nomeClasse == "FPortafoglio"
+            || $nomeClasse == "FScommessa" || $nomeClasse == "FSchedina") {
+            $verifica = $nomeClasse::update($campo, $nuovoValore, $chiave, $id);
+            return $verifica;
+        } else return false;
+    }
+
+    /**
+     * Metodo che accerta l'esistenza di un valore di un campo passato come parametro
+     * @param $campo da testare
+     * @param $vaoreCampo ,valore da cercare
+     * @param $nomeClasse, nome della classe Foundation interessata
+     */
+    public static function exist($campo, $valoreCampo, $nomeClasse) {
+        $risultato = null;
+        $risultato =  $nomeClasse::exist($campo, $valoreCampo);
+        return $risultato;
+    }
+
+    /*Metodo che permette di cancellare un commento dato l'id*/
+    public static function deleteCommento($id) {
+        $verifica = null;
+        $verifica = FCommento::deleteCommento($id);
+        return $verifica;
+    }
+}
+
+/**
+Metodo che permette di prelevare un utente registrato dal db
+richiamando però il metodo che verifica la corrispondenza della password inserita(in chiaro)
+tramite post e la password in chiaro nel db
+@param $username
+@param $pass è la password
+ */
+/*
+public static function loadLogin($username, $pass) {
+    $risultato = null;
+    $risultato = FUtenteRegistrato::loadLogin($username, $pass);
+    return $risultato;
+}
+*/
