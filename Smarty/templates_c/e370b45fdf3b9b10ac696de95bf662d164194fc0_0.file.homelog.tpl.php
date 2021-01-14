a55bec16c944922ce262be4edc4a92de99db6192
Digimatt <mattiadigiovanni98@gmail.com>
2021-01-14 18:23:38 +0100

    14/01/2021

--- Smarty/templates_c/e370b45fdf3b9b10ac696de95bf662d164194fc0_0.file.homelog.tpl.php
+++ Smarty/templates_c/e370b45fdf3b9b10ac696de95bf662d164194fc0_0.file.homelog.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2021-01-08 19:34:28
+/* Smarty version 3.1.34-dev-7, created on 2021-01-14 15:12:08
   from '/opt/lampp/htdocs/Progetto/Smarty/templates/homelog.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5ff8a5b46ecab5_77282558',
+  'unifunc' => 'content_600051388beb04_26730711',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e370b45fdf3b9b10ac696de95bf662d164194fc0' => 
     array (
       0 => '/opt/lampp/htdocs/Progetto/Smarty/templates/homelog.tpl',
-      1 => 1610130244,
+      1 => 1610633293,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ff8a5b46ecab5_77282558 (Smarty_Internal_Template $_smarty_tpl) {
+function content_600051388beb04_26730711 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 
@@ -310,7 +310,8 @@ $_smarty_tpl->tpl_vars['c']->first = $_smarty_tpl->tpl_vars['c']->iteration === 
 
                                              </a>
 
-                                             <a  class="pt-2" href="" >
+                                             <a  class="pt-2" href="/Progetto/SerieTv/info?id=<?php echo $_smarty_tpl->tpl_vars['serie']->value[$_smarty_tpl->tpl_vars['se']->value]->getId();?>
+" >
 
                                                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#555B5F" class="bi bi-binoculars" viewBox="0 0 16 16">
                                                      <path fill-rule="evenodd" d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
